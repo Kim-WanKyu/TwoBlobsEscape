0dfb216208c5bc848753b633f4efe01f39c40acf
Kim-WanKyu <qwertnmagicalmaze@gmail.com>
2024-05-20 03:18:08 +0900

    240515-240519 GameScene 관련.
    플레이어 애니메이션 설정. 점프, 착지 애니메이션 추가 및 적용.
    
    PlatformPhysicsMaterial2D 생성 및 오브젝트에 추가. 벽에 달라붙어서 걸으면 벽에 붙어버리는 현상 방지하려고 추가. 마찰력0으로 해서.
    
    Platform 레이어 추가 및 적용.
    
    PlayerController.cs 수정. 착지(LandingPlatform()) 추가.
    PlayerSwapper.cs 수정.
    플레이어 스왑 시 현재 플레이어를 이전 플레이어보다 앞으로 나오게 함. order in layer(sorting order)값을 0(이전) or 1(현재)로.
    이전 플레이어의 걷기 및 점프 애니메이션 종료.

--- TwoBlobsEscape/Assets/Player/Scripts/PlayerController.cs
+++ TwoBlobsEscape/Assets/Player/Scripts/PlayerController.cs
@@ -4,103 +4,113 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private PlayerSwapper playerSwapper;
+    private PlayerSwapper playerSwapper;    // 현재 플레이어를 가져오기 위해 추가.
 
-    private GameObject playerBlue;
-    private GameObject playerPink;
-
-    private Rigidbody2D rigidBlue;
-    private Rigidbody2D rigidPink;
-
-    private SpriteRenderer spriteRendererBlue;
-    private SpriteRenderer spriteRendererPink;
-
-    private Animator animBlue;
-    private Animator animPink;
-
-    private float speed;
-    private float jumpPower;
+    private float speed;        // 이동속도.
+    private float jumpPower;    //점프력.
 
     private void Awake()
     {
-        InitPlayerObject();
-
-        speed = 7f;
-        jumpPower = 20f;
-    }
-
-    private void InitPlayerObject()
-    {
         playerSwapper = GetComponent<PlayerSwapper>();
 
-        playerBlue = playerSwapper.GetPlayerBlue();
-        playerPink = playerSwapper.GetPlayerPink();
-
-        rigidBlue = playerBlue.GetComponent<Rigidbody2D>();
-        rigidPink = playerPink.GetComponent<Rigidbody2D>();
-
-        spriteRendererBlue = playerBlue.GetComponent<SpriteRenderer>();
-        spriteRendererPink = playerPink.GetComponent<SpriteRenderer>();
-
-        animBlue = playerBlue.GetComponent<Animator>();
-        animPink = playerPink.GetComponent<Animator>();
+        speed = 7f;
+        jumpPower = 21.5f;
     }
 
     private void Update()
     {
-        Move();
-
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKey(KeyCode.Space))
         {
-            Jump();
+            Jump(); // 플레이어 점프.
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move(); // 플레이어 이동.
+        LandingPlatform();  // 플레이어 착지.
+    }
+
+    // 플레이어 이동.
+    private void Move()
+    {
+        Vector2 direction = Vector2.zero;
+        // 키보드 입력 받아서 방향값을 설정.
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
         }
 
-        // Direction Sprite
-        if (Input.GetButton("Horizontal"))
-        {
-            if (playerSwapper.GetNowPlayer() == playerBlue)
-            {
-                spriteRendererBlue.flipX = Input.GetAxisRaw("Horizontal") == -1;
-            }
-            else if (playerSwapper.GetNowPlayer() == playerPink)
-            {
-                spriteRendererPink.flipX = Input.GetAxisRaw("Horizontal") == -1;
-            }
-        }
+        // 현재 플레이어의 속도를 설정한 방향값에 이동속도로 설정하여, 현재 플레이어를 이동시키는 코드.
+        playerSwapper.GetNowPlayer().GetComponent<Rigidbody2D>().velocity = new Vector2(direction.x * speed, playerSwapper.GetNowPlayer().GetComponent<Rigidbody2D>().velocity.y);
 
-        // Animation walk
-        if (Input.GetButton("Horizontal"))
+        // 걷기 애니메이션.
+        // 방향값이 있으면 이동 중이므로, 걷기 애니메이션 실행.
+        if (direction != Vector2.zero)
         {
-            if (playerSwapper.GetNowPlayer() == playerBlue)
-            {
-                animBlue.SetBool("isWalking", true);
-            }
-            else if (playerSwapper.GetNowPlayer() == playerPink)
-            {
-                animPink.SetBool("isWalking", true);
-            }
+            playerSwapper.GetNowPlayer().GetComponent<SpriteRenderer>().flipX = (direction == Vector2.left);    // 스프라이트 방향 설정. 플레이어의 방향이 왼쪽이면 스프라이트를 flip함.
+            playerSwapper.GetNowPlayer().GetComponent<Animator>().SetBool("isWalking", true);
         }
         else
         {
-            if (playerSwapper.GetNowPlayer() == playerBlue)
-            {
-                animBlue.SetBool("isWalking", false);
-            }
-            else if (playerSwapper.GetNowPlayer() == playerPink)
-            {
-                animPink.SetBool("isWalking", false);
-            }
+            // 그렇지 않으면, 정지이므로, 걷기 애니메이션 끄기.
+            playerSwapper.GetNowPlayer().GetComponent<Animator>().SetBool("isWalking", false);
         }
     }
 
-    private void Move()
-    {
-        float h = Input.GetAxisRaw("Horizontal");
-        playerSwapper.GetNowPlayer().transform.Translate(new Vector2(h * speed, 0) * Time.deltaTime);
-    }
-
+    // 플레이어 점프.
     private void Jump()
     {
-        playerSwapper.GetNowPlayer().GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        // 애니메이션의 "isJumping"값이 false이면, -> 점프 중이 아님(공중에 있지 않음).
+        if (playerSwapper.GetNowPlayer().GetComponent<Animator>().GetBool("isJumping") == false)
+        {
+            // 플레이어를 위의 방향으로 힘을 주어 점프 시킴.
+            playerSwapper.GetNowPlayer().GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            // 점프 애니메이션 실행.
+            playerSwapper.GetNowPlayer().GetComponent<Animator>().SetBool("isJumping", true);
+        }
     }
+    
+    // 플레이어 착지.
+    private void LandingPlatform()
+    {
+        // 바닥 감지 영역을 위한 값들 초기화.
+        float playerSizeX = playerSwapper.GetNowPlayer().GetComponent<Collider2D>().bounds.size.x;  // 감지 영역 너비를 플레이어의 콜라이더의 x 크기로 설정.
+        Vector2 hitPos = playerSwapper.GetNowPlayer().transform.position;   // 감지 영역 위치.
+        Vector2 hitSize = new Vector2(playerSizeX, 0.2f);                   // 감지 영역 크기.
+
+        // BoxCast로 박스모양의 영역으로 raycasthit. (레이어가 Platform인 오브젝트만 감지)
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(hitPos, hitSize, 0f, Vector2.right, 0, LayerMask.GetMask("Platform"));
+
+        // 플레이어의 y속력이 0보다 작을 때 -> 플레이어가 떨어지고 있을 때에만 조건을 검사.
+        if (playerSwapper.GetNowPlayer().GetComponent<Rigidbody2D>().velocity.y < 0)
+        {
+            // 바닥을 밟으면, collider가 null이 아님.
+            if (raycastHit2D.collider != null)
+            {
+                Debug.Log("standing platform is : " + raycastHit2D.collider.name);
+                // 바닥을 밟으면, 애니메이션의 "isJumping"값을 false로 설정.
+                playerSwapper.GetNowPlayer().GetComponent<Animator>().SetBool("isJumping", false);
+                // 바닥을 밟으면, 플레이어의 속도를 0으로.
+                playerSwapper.GetNowPlayer().GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            }
+            else
+            {
+                // 바닥을 밟지 않으면, -> 공중이면, 애니메이션의 "isJumping"값을 true로 설정. -> 점프가 아닌 추락일 때도 점프로 처리하기 위함. -> 추락 시에도 착지 애니메이션 실행됨.
+                playerSwapper.GetNowPlayer().GetComponent<Animator>().SetBool("isJumping", true);
+            }
+        }
+
+        // (Debug) 충돌 감지 시각화를 위한 DrawRay. 바닥을 밟으면 녹색, 아니면 빨간색으로 씬에서 볼 수 있음.
+        Color hitColor = (raycastHit2D.collider != null ? Color.green : Color.red);
+        Debug.DrawRay(hitPos + new Vector2(-playerSizeX / 2f, 0.1f), Vector2.down * 0.2f, hitColor);
+        Debug.DrawRay(hitPos + new Vector2(playerSizeX / 2f, 0.1f), Vector2.down * 0.2f, hitColor);
+        Debug.DrawRay(hitPos + new Vector2(playerSizeX / 2f, 0.1f), Vector2.left * playerSizeX, hitColor);
+        Debug.DrawRay(hitPos + new Vector2(playerSizeX / 2f, -0.1f), Vector2.left * playerSizeX, hitColor);
+    }
+
 }
--- TwoBlobsEscape/Assets/Player/Scripts/PlayerSwapper.cs
+++ TwoBlobsEscape/Assets/Player/Scripts/PlayerSwapper.cs
@@ -5,31 +5,29 @@ using UnityEngine;
 
 public class PlayerSwapper : MonoBehaviour
 {
-    [SerializeField] private GameObject playerBlue; // 파랑.
-    [SerializeField] private GameObject playerPink; // 분홍.
-    private GameObject nowPlayer;   // 현재 플레이어.
+    [SerializeField] private GameObject playerBlue; // 파랑이.
+    [SerializeField] private GameObject playerPink; // 분홍이.
 
-    public GameObject GetPlayerBlue() { return playerBlue; }
-    public GameObject GetPlayerPink() { return playerPink; }
+    private GameObject nowPlayer;   // 현재 플레이어.
     public GameObject GetNowPlayer() { return nowPlayer; }
 
     // 플레이어의 활성화/비활성화 된 색
     private Color activatedColor;
     private Color deactivatedColor;
 
-
     void Awake()
     {
         // 플레이어의 활성화/비활성화 된 색 초기화.
-        activatedColor = new Color(1, 1, 1);
-        deactivatedColor = new Color(100 / 255f, 100 / 255f, 100 / 255f);
+        activatedColor = new Color(1, 1, 1);    // 컬러는 0~1이므로, (1,1,1)는 흰색.
+        deactivatedColor = new Color(100 / 255f, 100 / 255f, 100 / 255f); // 0~255를 255로 나누어 0~1로 정규화. (회색).
 
-        // 각 플레이어의 색을 초기화. (파랑이를 활성화, 분홍이를 비활성화로).
+        // 각 플레이어의 색을 초기화. (파랑이를 활성화 / 분홍이를 비활성화 된 색으로).
         playerBlue.GetComponent<SpriteRenderer>().color = activatedColor;
         playerPink.GetComponent<SpriteRenderer>().color = deactivatedColor;
 
-        // 현재 플레이어를 파랑이로 설정.
+        // 현재 플레이어를 파랑이로 설정하고, 값들을 초기화.
         nowPlayer = playerBlue;
+        nowPlayer.GetComponent<SpriteRenderer>().sortingOrder = 1;
     }
 
     void Update()
@@ -37,16 +35,29 @@ public class PlayerSwapper : MonoBehaviour
         // C키(플레이어 변경 키) 누르면,
         if (Input.GetKeyDown(KeyCode.C))
         {
-            // 이전 플레이어의 걷기 애니메이션 종료.
-            nowPlayer.GetComponent<Animator>().SetBool("isWalking", false);
-            // 이전 플레이어의 색을 비활성화된 색으로 변경.
-            nowPlayer.GetComponent<SpriteRenderer>().color = deactivatedColor;
-
-            // 플레이어 변경.
-            nowPlayer = nowPlayer == playerBlue ? playerPink : playerBlue;
-
-            // 현재 플레이어의 색을 활성화된 색으로 변경.
-            nowPlayer.GetComponent<SpriteRenderer>().color = activatedColor;
+            SwapPlayer();
         }
+    }
+
+    // 플레이어 스왑(파랑이 <-> 분홍이).
+    void SwapPlayer()
+    {
+        // 이전 플레이어의 속도를 0으로 설정.
+        nowPlayer.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        // 이전 플레이어의 애니메이션들 종료.
+        nowPlayer.GetComponent<Animator>().SetBool("isWalking", false); // 걷기.
+        nowPlayer.GetComponent<Animator>().SetBool("isJumping", false); // 점프.
+        // 이전 플레이어의 색을 비활성화된 색으로 변경.
+        nowPlayer.GetComponent<SpriteRenderer>().color = deactivatedColor;
+        // 이전 플레이어의 Order in Layer를 0으로 변경(이전 플레이어가 현재 플레이어의 뒤로 가도록).
+        nowPlayer.GetComponent<SpriteRenderer>().sortingOrder = 0;
+
+        // 플레이어 변경.
+        nowPlayer = ((nowPlayer == playerBlue) ? playerPink : playerBlue);
+
+        // 현재 플레이어의 색을 활성화된 색으로 변경.
+        nowPlayer.GetComponent<SpriteRenderer>().color = activatedColor;
+        // 현재 플레이어의 Order in Layer를 1으로 변경(현재 플레이어가 이전 플레이어의 앞으로 가도록).
+        nowPlayer.GetComponent<SpriteRenderer>().sortingOrder = 1;
     }
 }
