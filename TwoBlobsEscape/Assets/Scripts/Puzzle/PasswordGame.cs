eba172c5009fcc8a14b1bf4a82fe209e2d35fdda
Kim-WanKyu <qwertnmagicalmaze@gmail.com>
2024-06-16 00:38:35 +0900

    240615 비밀번호 퍼즐 버그 수정&보완 및 주석 작성.
    PasswordGame.cs 수정.
    정답이 4자리 수가 아닌 경우,
    숫자를 맞추었음에도 정답 처리가 되지 않는 버그 수정.
    ToString().PadLeft() 활용.
    
    추가로 4자리 고정이 아닌 슬롯을 추가/감소 하는 경우,
    동적으로 정수의 범위를 자리수를 슬롯의 개수 만큼으로 수정.

--- TwoBlobsEscape/Assets/Scripts/Puzzle/PasswordGame.cs
+++ TwoBlobsEscape/Assets/Scripts/Puzzle/PasswordGame.cs
@@ -67,9 +67,9 @@ public class PasswordGame : Puzzle
     // 텍스트 초기화 메소드.
     private void InitTexts()
     {
-        questionText.text = MakeQuestion();
+        questionText.text = MakeQuestion(); // 문제는 만들어준 값으로 초기화하고,
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)  // 슬롯은 '0' 으로 초기화.
         {
             slotTexts[i].text = "0";
         }
@@ -78,15 +78,14 @@ public class PasswordGame : Puzzle
     // 문제 만드는 메소드.
     private string MakeQuestion()
     {
-        int questionValue = Random.Range(0, 10000); // 정답이 될 0~9999의 랜덤한 정수를 만들고,
+        int questionValue = UnityEngine.Random.Range(0, (int)Mathf.Pow(10, size));    // 정답이 될 0 ~ 10^size(4)-1(=9999)의 랜덤한 정수를 만들어
 
-        answer = questionValue.ToString();  // 정답 변수에 넣어주고,
+        answer = questionValue.ToString().PadLeft(size, '0');  // 부족한 부분은 0으로 채운 size의 길이만큼의 문자열을 만들어준 다음, 정답 변수에 넣어주고,
 
-        int a = Random.Range(0, questionValue);     // a와 b 두 부분으로 랜덤하게 나누어주어,
-        int b = questionValue - a;
+        int a = UnityEngine.Random.Range(0, questionValue);    // a와 b 두 부분으로 랜덤하게 나누어주어,
+        int b = questionValue - a;                             // a + b = answer 가 되도록 해줌.
 
-
-        return $"{a} + {b} = ?";                    // "a+b=?" 형식의 텍스트를 만들어 리턴.
+        return $"{a} + {b} = ?";        // "a + b = ?" 형식의 문자열을 만들어 리턴.
     }
 
 
@@ -96,15 +95,15 @@ public class PasswordGame : Puzzle
         sfxManager = GameManager.instance.gameObject.transform.GetChild(0).GetComponent<SfxManager>();
         sfxManager.PlayButtonSound();
 
-        GameObject go = EventSystem.current.currentSelectedGameObject;  // EventSystem.current.currentSelectedGameObject 마우스로 선택된 오브젝트 반환.
+        GameObject go = EventSystem.current.currentSelectedGameObject;  // EventSystem.current.currentSelectedGameObject 마우스로 선택된 오브젝트(UI) 가져오기. // https://alpaca-code.tistory.com/139 참고.
 
-        Text t = go.transform.parent.GetChild(2).GetChild(0).GetComponent<Text>();
-        int currentNum = int.Parse(t.text);
-        currentNum = (currentNum + 1) % 10;
+        Text t = go.transform.parent.GetChild(2).GetChild(0).GetComponent<Text>();  // 현재 선택된 버튼이 조작하는 슬롯의 텍스트.
+        int currentNum = int.Parse(t.text); // 텍스트의 정수문자를 정수로 변환. 
+        currentNum = (currentNum + 1) % 10;    // 현재 정수값에 1을 더해주고 10으로 mod 연산. (9 -> 0 -> 1)
 
-        t.text = currentNum.ToString();
+        t.text = currentNum.ToString(); // 1을 더해준 값을 다시 문자열로 변환하여 슬롯의 텍스트로 설정.
 
-        CheckClear();
+        CheckClear();   // 정답인지 확인.
     }
 
     // 해당 슬롯의 숫자를 하나 내리는 메소드.
@@ -113,11 +112,11 @@ public class PasswordGame : Puzzle
         sfxManager = GameManager.instance.gameObject.transform.GetChild(0).GetComponent<SfxManager>();
         sfxManager.PlayButtonSound();
 
-        GameObject go = EventSystem.current.currentSelectedGameObject;  // EventSystem.current.currentSelectedGameObject 마우스로 선택된 오브젝트 반환.
+        GameObject go = EventSystem.current.currentSelectedGameObject;  // EventSystem.current.currentSelectedGameObject 마우스로 선택된 오브젝트(UI) 가져오기.
 
         Text t = go.transform.parent.GetChild(2).GetChild(0).GetComponent<Text>();
         int currentNum = int.Parse(t.text);
-        currentNum = (currentNum - 1 + 10) % 10;
+        currentNum = (currentNum - 1 + 10) % 10;    // 현재 정수값에 1을 빼주고 10으로 mod 연산. (1 -> 0 -> 9)
 
         t.text = currentNum.ToString();
 
@@ -131,10 +130,10 @@ public class PasswordGame : Puzzle
 
         for (int i = 0; i < size; i++)
         {
-            sb.Append(slotTexts[i].text);
+            sb.Append(slotTexts[i].text); // StringBuilder 객체의 Append() 를 활용하여, 문자열을 연결해줌. string의 "+" 연산의 메모리 문제 방지.
         }
         
-        return sb.ToString();
+        return sb.ToString();   // 문자열로 변환하기 위해 ToString() 처리.
     }
 
     public override void InitPosSet()
@@ -145,7 +144,7 @@ public class PasswordGame : Puzzle
 
     public override void CheckClear()
     {
-        if (answer.CompareTo(GetSlotNumbers()) == 0)  // 질문의 답과 슬롯의 답이 같으면, 정답.
+        if (answer.CompareTo(GetSlotNumbers()) == 0)  // 질문의 답과 슬롯의 답 문자열이 같은지 비교하고, 같으면 정답 처리.
         {
             sfxManager.PlayPuzzleClearSound();
 
