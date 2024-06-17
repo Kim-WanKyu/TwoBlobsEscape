fea8927c87f6e2fa05fce5845111208202c11b2f
Kim-WanKyu <qwertnmagicalmaze@gmail.com>
2024-06-17 09:10:10 +0900

    240617 GameManager.cs 수정
    프로그램 종료 시 PlayerPrefs에 저장된 모든 데이터 삭제하는 구문 주석 처리

--- TwoBlobsEscape/Assets/Scripts/GameManager.cs
+++ TwoBlobsEscape/Assets/Scripts/GameManager.cs
@@ -103,9 +103,9 @@ public class GameManager : MonoBehaviour
     // 게임 종료. (프로그램 종료)
     public void GameQuit()
     {
-        Debug.Log("모든 시간 기록 삭제");
+        //Debug.Log("모든 시간 기록 삭제");
         //PlayerPrefs에 저장된 모든 데이터 삭제.
-        PlayerPrefs.DeleteAll();
+        //PlayerPrefs.DeleteAll();
 
         Debug.Log("게임종료");
         // 프로그램 종료.
